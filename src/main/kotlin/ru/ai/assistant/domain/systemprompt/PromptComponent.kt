26c79407134de5f79234244d6287a31d48e40ab1
Pushkarev Nikolay <nikolazmailz@gmail.com>
2025-10-17 15:00:05 +0300

    refactoring

--- src/main/kotlin/ru/ai/assistant/domain/systemprompt/PromptComponent.kt
+++ src/main/kotlin/ru/ai/assistant/domain/systemprompt/PromptComponent.kt
@@ -39,7 +39,7 @@ class PromptComponent(
                 "${dialogInfo.levelOfResponseCompleteness?.levelPrompt} \n " +
                 "$globalPromptContent \n Твои знания: $knowledge \n" +
                 "".let {
-                    log.debug { "\n\n collectSystemPrompt $it \n\n" }
+                    log.debug { "\n\n collectSystemPrompt \n $it \n\n" }
                     it
                 }
     }
