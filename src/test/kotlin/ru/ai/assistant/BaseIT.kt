1db981a96472547d908b6b0ef08619f9c8a10092
nikolazmailz <nikolazmailz@gmail.com>
2025-10-18 10:07:29 +0300

    Refactor Telegram webhook IT to use BaseIT

--- src/test/kotlin/ru/ai/assistant/BaseIT.kt
+++ src/test/kotlin/ru/ai/assistant/BaseIT.kt
@@ -4,6 +4,7 @@ import com.ninjasquad.springmockk.MockkBean
 import com.ninjasquad.springmockk.MockkBeans
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.ShouldSpec
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -12,12 +13,11 @@ import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
-import ru.ai.assistant.controller.TelegramWebhookController
 import ru.ai.assistant.application.handler.AiAssistantHandler
 import ru.ai.assistant.infra.openapi.OpenAISenderImpl
 
 @SpringBootTest(
-    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     classes = [TestWebClientConfig::class],
     properties = [
         "spring.main.allow-bean-definition-overriding=true",
@@ -27,9 +27,9 @@ import ru.ai.assistant.infra.openapi.OpenAISenderImpl
 )
 @ActiveProfiles("test")
 @Testcontainers
+@AutoConfigureWebTestClient
 @MockkBeans(
-    MockkBean(TelegramWebhookController::class),
-//    MockkBean(OpenAIWebClientConfig::class),
+    //    MockkBean(OpenAIWebClientConfig::class),
     MockkBean(AiAssistantHandler::class),
     MockkBean(OpenAISenderImpl::class),
 )
